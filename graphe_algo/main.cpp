cfddc37f08c4c32cd13d25fc57debc917cc98d8f
Soufyane <tarrimi.soufyane@hotmail.fr>
2020-04-03 00:29:44 +0200

    PRUFER MAAAAARCHE ENCORE

--- graphe_algo/main.cpp
+++ graphe_algo/main.cpp
@@ -12,12 +12,15 @@ int main(int argc, char *argv[])
 {
    QApplication a(argc, argv);
    MainWindow w;
-  /* std::vector<std::vector<int>>C{{0,0,0,0,0,0,0},{0,0,10,3,MAXPOIDS,6,MAXPOIDS},{0,0,0,MAXPOIDS,MAXPOIDS,MAXPOIDS,MAXPOIDS},{0,MAXPOIDS,4,0,MAXPOIDS,2,MAXPOIDS},{0,MAXPOIDS,MAXPOIDS,1,0,3,MAXPOIDS},{0,MAXPOIDS,0,MAXPOIDS,MAXPOIDS,0,1},{0,2,1,MAXPOIDS,MAXPOIDS,MAXPOIDS,0}};
+   /*std::vector<std::vector<int>>C{{0,0,0,0,0,0,0},{0,0,10,3,MAXPOIDS,6,MAXPOIDS},{0,0,0,MAXPOIDS,MAXPOIDS,MAXPOIDS,MAXPOIDS},{0,MAXPOIDS,4,0,MAXPOIDS,2,MAXPOIDS},{0,MAXPOIDS,MAXPOIDS,1,0,3,MAXPOIDS},{0,MAXPOIDS,0,MAXPOIDS,MAXPOIDS,0,1},{0,2,1,MAXPOIDS,MAXPOIDS,MAXPOIDS,0}};
    std::vector<int>fs{19,2,3,5,0,1,0,2,5,0,3,5,0,2,6,0,1,2,0};
    std::vector<int>aps{6,1,5,7,10,13,16};
    std::vector<std::vector<int>> matD(7,std::vector<int>(7,0));
    fs_aps g(fs,aps);
    matD=g.Dijkstra(C);
+   for(int i=0; i<g.getAps()[0];i++)
+       for(int j=0;i<g.getAps()[0];j++)
+       cout<<matD[i][j];
 
    std::vector<int>fss{21,2,3,0,4,0,5,8,0,6,2,0,3,7,0,1,8,0,8,0,7,0};
    std::vector<int>apss{8,1,4,6,9,12,15,18,20};
@@ -30,30 +33,41 @@ int main(int argc, char *argv[])
    {
        QTextStream(stdout)<<"CFC :"<<t[i]<<" ";
    }
-   QTextStream(stdout)<<"\n";*/
-
+   QTextStream(stdout)<<"\n";
    std::vector<int>fss{21,2,3,0,4,0,5,8,0,6,2,0,3,7,0,1,8,0,8,0,7,0};
    std::vector<int>apss{8,1,4,6,9,12,15,18,20};
+   std::vector<int>fsss{12,4,0,4,4,0,4,0,4,0,5,0,6,0,0};
+   std::vector<int>apsss{6,1,3,5,7,9,11};
+
+
    fs_aps g(fss,apss);
    std::vector<int> dist;
    std::vector<int> pred;
 
-   /*g.calcul_dist(1,dist,pred);
+   g.calcul_dist(2,dist,pred);
 
    for(int i=0;i<dist.size();i++)
        std::cout<<dist[i]<<"!";
    std::cout<<endl;
+
    for(int i=0;i<pred.size();i++)
-       std::cout<<pred[i]<<"!";*/
+       std::cout<<pred[i]<<"!";
+*/
+   std::vector<int>fsss{12,4,0,4,0,4,0,5,0,6,0,0};
+   std::vector<int>apsss{6,1,3,5,7,9,11};
+   fs_aps h(fsss,apsss);
+   matrice_Adjacence m(h);
+   std::vector<int> d;
 
-   matrice_Adjacence m(g);
-   pred=m.codage_Pruffer();
+   m.codage_Pruffer(d);
    for(int i=0;i<m.getMatrice()[0][0];i++){
-       cout<<pred[i]<<"//";
-
+       cout<<d[i]<<"//";
    }
+
+
 
 
    w.show();
    a.exec();
+   return 0;
 }
--- graphe_algo/matrice_adjacence.cpp
+++ graphe_algo/matrice_adjacence.cpp
@@ -18,7 +18,6 @@ matrice_Adjacence::matrice_Adjacence(fs_aps &graphFsAps) {
     d_matrice[0][1]=d_nbArcs;
     for(int i=1; i<=d_nbSommet;i++) {
         int j;
-        std::cout<<"FOR";
         for(int k=graphFsAps.getAps()[i];(j=graphFsAps.getFs()[k])!=0;k++){
             d_matrice[i][j]=1;
 
@@ -30,38 +29,41 @@ std::vector<std::vector<int>> matrice_Adjacence::getMatrice()
     return d_matrice;
 }
 
-std::vector<int> matrice_Adjacence::codage_Pruffer() {
-    int m=d_nbSommet;
-    std::vector<int> prufer(m-1,0);
-    prufer[0]=m-2;
-    int A[m+1][m+1];
-    for (int i = 0; i <= m; ++i){
-        for (int j = 0; j <= m; ++j){
-            A[i][j] = d_matrice[i][j];
+void matrice_Adjacence::codage_Pruffer(std::vector<int> &X){
+    int m =d_matrice[0][0];
+    X.resize(m+1);
+    for(int i=1; i<=m; i++){
+
+        for(int j=1; j<=m ; j++){;
+
+            if(d_matrice[i][j]==1){
+
+                d_matrice[i][0]++;
+            }
         }
     }
-    int compteur;
-    for (int i = 1; i <= m; ++i){
-       compteur = 0;
-       for (int j = 1; j <=m; ++j){
-           if (A[i][j] == 1){
-               compteur++;
-           }
+    X[0] = m-2;
+    int i=1;
+
+    for(int k=1 ; k<=m-2 ; k++){
+        for(int i=1 ; i<=m ; i++){
+
+            if(d_matrice[i][0] == 1){
+
+                d_matrice[i][0] = 0;
+            }
         }
-             A[i][0] = compteur;
+        for(int j=1 ; j<=m ; j++){
+
+            if( d_matrice[i][j] ==1){
+
+                X[k] = j;
+                 d_matrice[i][j] = 0;
+                 d_matrice[j][i] = 0;
+                 d_matrice[j][0]--;
+            }
+        }
+        i++;
     }
-    for (int i = 1; i <= m-2; ++i){
-       int j = 1;
-       while (j <= m && A[j][0] != 1){
-           ++j;
-           A[j][0] = 0; int k = 1;
-           while (k <= m && A[j][k] != 1){
-                  ++k;
-             A[j][k] = 0; A[k][j] = 0; A[k][0]--;
-             prufer[i] = k;
-           }
-       }
-    }
-    return prufer;
 }
 
